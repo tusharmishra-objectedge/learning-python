226e349b0879df1decc331d1b8211ef689ca38b5
Tushar Mishra <tushar.mishra@objectedge.com>
2023-01-25 17:28:43 +0530

    added code snippets for common python concepts

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+# basic concepts used in python
+
+
+# for loop
+fruits = ['apple', 'banana', 'cherry']
+for x in fruits:
+    print(x)
+
+# conditionals
+x = 5
+if x > 0:
+    print("x is positive")
+else:
+    print("x is negative")
+
+
+# functions
+def add(x, y):
+    return x + y
+
+
+result = add(5, 3)
+print(result)
+
+
+# classes
 class Dog:
     def __init__(self, name, age):
         self.name = name
@@ -8,13 +34,76 @@ dog1 = Dog("Fido", 5)
 print(dog1.name)
 print(dog1.age)
 
+# exception handling
+try:
+    x = 5 / 0
+except ZeroDivisionError:
+    print("Cannot divide by zero")
 
-x = int(input())
-if x > 0:
-    print(f"{x} is positive (+ve)")
-    if x % 2:
-        print(f"{x} is even")
-    else:
-        print(f"{x} is odd")
-else:
-    print(f"{x} is negative (-ve)")
+# list comprehension
+nums = [1, 2, 3, 4, 5]
+squared_nums = [x ** 2 for x in nums]
+print(squared_nums)
+
+# lambda
+double = lambda x: x * 2
+result = double(5)
+print(result)
+
+# map & filter
+nums = [1, 2, 3, 4, 5]
+squared_nums = map(lambda x: x ** 2, nums)
+filtered_nums = filter(lambda x: x > 2, squared_nums)
+print(list(filtered_nums))
+
+# dictionary
+person = {"name": "John", "age": 30, "city": "New York"}
+print(person["name"])
+
+# tuple
+fruits = ("apple", "banana", "cherry")
+print(fruits[1])
+
+# set
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+print(fruits)
+
+# sorting
+nums = [3, 1, 4, 2, 5]
+nums.sort()
+print(nums)
+
+
+# generator
+def fibonacci(n):
+    a, b, counter = 0, 1, 0
+    while True:
+        if (counter > n):
+            return
+        yield a
+        a, b = b, a + b
+        counter += 1
+
+
+f = fibonacci(5)
+for x in f:
+    print(x, end=" ")
+
+
+# decorators
+def my_decorator(func):
+    def wrap_func(*args, **kwargs):
+        print("**********")
+        func(*args, **kwargs)
+        print("**********")
+
+    return wrap_func
+
+
+@my_decorator
+def hello(name):
+    print(f"Hello, {name}")
+
+
+hello("John")
