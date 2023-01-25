0659f776460b10e53afa0f584aa160804a6e1802
Tushar Mishra <tushar.mishra@objectedge.com>
2023-01-25 13:09:00 +0530

    pep followed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 class Dog:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
 
 dog1 = Dog("Fido", 5)
 print(dog1.name)
