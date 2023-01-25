8e4cc2de1dd0bf2c1b8005be7da31d97d5391696
Tushar Mishra <tushar.mishra@objectedge.com>
2023-01-25 17:01:36 +0530

    undo

--- main_BASE_252.py
+++
@@ -1,5 +0,0 @@
-x = 5
-if x > 0:
-    print("x is positive")
-else:
-    print("x is negative")
--- main_LOCAL_252.py
+++
@@ -1,9 +0,0 @@
-class Dog:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-
-dog1 = Dog("Fido", 5)
-print(dog1.name)
-print(dog1.age)
