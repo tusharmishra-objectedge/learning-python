f31ef31df407430d46d1c16d4e74b9625dab5ba6
Tushar Mishra <tushar.mishra@objectedge.com>
2023-01-25 13:02:53 +0530

    resolved conflicts and added a class

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-x = 5
-if x > 0:
-    print("x is positive")
-else:
-    print("x is negative")
+class Dog:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+dog1 = Dog("Fido", 5)
+print(dog1.name)
+print(dog1.age)
