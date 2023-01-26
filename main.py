eb205587e4fe71eb6aa0c1723f5b04455ddf7917
Tushar Mishra <tushar.mishra@objectedge.com>
2023-01-26 12:36:18 +0530

    cleared test

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-x = int(input())
-if x > 0:
-    print(f"{x} is positive (+ve)")
-    if x % 2:
-        print(f"{x} is even")
-    else:
-        print(f"{x} is odd")
-else:
-    print(f"{x} is negative (-)")
-    def g():
-        print(None)
-        return False
-    print('not a natural no.')
+'''
+nothing
+new
+here
+'''
